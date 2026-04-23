9411c097a939b3b918b07fb902c3c748c075ad4b
fidelrodriguez1688 <Fidel.Rodriguezet32@gmail.com>
2026-04-23 01:53:52 -0300

    Add files via upload

---
+++ Pipilibre/Vistas/registro/vista_registro.php
@@ -0,0 +1,49 @@
+<?php
+ session_start()
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro de Usuarios</title>
+    <link rel="stylesheet" href="Registro.css">
+</head>
+<body>
+
+    <div class="contenedor-fondo"> 
+        <!-- logo -->
+        <div class="barra_superior">
+            <img class="logowich_arriba" src="../../imagenes\reallogo.png" alt="">
+        </div>
+        <!-- el formulario del madafakin profe -->
+        <div class="container">
+            <h2>Registrar Nuevo Usuario</h2>
+            <form action="registro.php" method="POST">
+                <!-- -->
+                <label>Ingrese nombre de usuario:</label>
+                <?php if (isset($_SESSION['trigger_regirtro_error'])): ?>
+                        <div class="alerta_verde_afuera">
+                           <div class="alerta_verde_dentro">
+                                !Cuenta o mail ya existente¡ o hay carcater invalido
+                           </div>
+                        </div>
+                <?php 
+                   unset($_SESSION['trigger_regirtro_error']); 
+                   endif; 
+                 ?>
+                 <p></p>
+                <input type="text" name="Nombre" placeholder="ej:juan123" required>
+                 <!-- -->
+                <label>Ingrese su Mail:</label>
+                <input type="email" name="correo_electronico" placeholder="juan@ejemplo.com" required>
+                <!-- -->
+                <label>Contraseña:</label>
+                <input type="password" name="contraseña" placeholder="contraseañ segura" required>
+                <button type="submit">Registrase</button>
+            </form>
+               <h2></h2>¿ya tienes una cuenta?<a href="../inicio/inicio.php">inicia sesion</a></h2>
+        </div>
+    </div>
+</body>
+</html>
